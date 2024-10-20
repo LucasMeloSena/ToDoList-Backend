2a2bc779119cfb54cc23395213103511e8ac8e18
Lucas Sena <lucasmelosena@gmail.com>
2024-10-20 20:20:02 -0300

    test: create `generate and validate token` unit tests

--- src/test/java/br/dev/lucasena/todolist/core/cases/auth/GenerateTokenUseCaseTest.java
+++ src/test/java/br/dev/lucasena/todolist/core/cases/auth/GenerateTokenUseCaseTest.java
@@ -1,12 +1,52 @@
 package br.dev.lucasena.todolist.core.cases.auth;
 
+import br.dev.lucasena.todolist.domain.user.User;
+import br.dev.lucasena.todolist.domain.user.UserDTO;
+import br.dev.lucasena.todolist.repositories.IUserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+@ActiveProfiles("test")
 class GenerateTokenUseCaseTest {
+    @Mock
+    private IUserRepository userRepository;
+
+    @Autowired
+    @InjectMocks
+    private GenerateTokenUseCase generateTokenUseCase;
+
+    @BeforeEach
+    void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
 
     @Test
-    void execute() {
+    @DisplayName("should generate token successfully")
+    void execute1() throws Exception {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setName("John Doe");
+        userDTO.setEmail("johdoe@email.com");
+        userDTO.setPassword("123456");
+        User user = new User(userDTO);
+        user.setId(UUID.randomUUID());
+
+        when(userRepository.save(user)).thenReturn(user);
+
+        String token = generateTokenUseCase.execute(user);
+
+        assertNotNull(token);
+
+        verify(userRepository, times(1)).save(user);
     }
 }
--- src/test/java/br/dev/lucasena/todolist/core/cases/auth/ValidateTokenUseCaseTest.java
+++ src/test/java/br/dev/lucasena/todolist/core/cases/auth/ValidateTokenUseCaseTest.java
@@ -1,12 +1,56 @@
 package br.dev.lucasena.todolist.core.cases.auth;
 
+import br.dev.lucasena.todolist.domain.user.User;
+import br.dev.lucasena.todolist.domain.user.UserDTO;
+import br.dev.lucasena.todolist.repositories.IUserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+@ActiveProfiles("test")
 class ValidateTokenUseCaseTest {
+    @Mock
+    private IUserRepository userRepository;
+
+    @Autowired
+    @InjectMocks
+    private GenerateTokenUseCase generateTokenUseCase;
+    @Autowired
+    @InjectMocks
+    private ValidateTokenUseCase validateTokenUseCase;
+
+    @BeforeEach
+    void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
 
     @Test
-    void execute() {
+    @DisplayName("should validate token successfully")
+    void execute1() {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setName("John Doe");
+        userDTO.setEmail("johdoe@email.com");
+        userDTO.setPassword("123456");
+        User user = new User(userDTO);
+        user.setId(UUID.randomUUID());
+
+        when(userRepository.save(user)).thenReturn(user);
+
+        String token = generateTokenUseCase.execute(user);
+        String test = validateTokenUseCase.execute(token);
+
+        assertNotNull(test);
+
+        verify(userRepository, times(1)).save(user);
     }
 }
