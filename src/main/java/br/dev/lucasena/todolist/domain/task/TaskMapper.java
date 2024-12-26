14fff867eb4a4c11bc64f316f68aa11e1b4d52be
Lucas Sena <lucasmelosena@gmail.com>
2024-12-26 10:40:12 -0300

    refactor: make adjustments in task mapper

--- src/main/java/br/dev/lucasena/todolist/domain/task/TaskMapper.java
+++ src/main/java/br/dev/lucasena/todolist/domain/task/TaskMapper.java
@@ -1,12 +1,10 @@
 package br.dev.lucasena.todolist.domain.task;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface TaskMapper {
-    @Mapping(target = "name", source = "name")
     void updateTaskFromDto(TaskDTO dto, @MappingTarget Task entity);
 }
